cf74af8250c362e9745177da259adb1e1662bd5f
Camille <camillemsierra@gmail.com>
2021-02-01 20:06:30 -0800

    cleaned up console and names

--- src/Madlibz/Madlibz.js
+++ src/Madlibz/Madlibz.js
@@ -4,79 +4,73 @@ class Madlibz extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      result: '',
+      story: '',
       showErrors: true,
-    }
-  }
+    };
+  };
 
   newGame = async () => {
     const res = await fetch('http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25')
     if (!res.ok) {
       const error = await res.text();
       console.log(error);
-    }
-    const data = await res.json()
+    };
+    const data = await res.json();
     if (data) {
       this.setState(() => ({
         title: data.title,
         blanks: data.blanks,
-        valuesGiven: data.value,
-        result: '',
+        sentences: data.value,
+        story: '',
         value: [],
-        error: [],
+        showErrors: true,
       }));
-    }
-  }
+    };
+  };
 
   componentDidMount = async () => {
-    this.newGame()
-  }
+    this.newGame();
+  };
 
   handleChange = (event, index) => {
     let input = event.target.value;
     const newValue = [...this.state.value];
-    newValue[index] = input
+    newValue[index] = input;
     this.setState({ value: newValue });
-  }
+  };
 
   handleSubmit = () => {
-    // let result = [];
-    // for (let i = 0; i < this.state.valuesGiven.length - 1; i++) {
-    //   if (!this.state.value[i]) {
-    //     this.setState(() => ({
-    //       error: 'please fill this out'
-    //     }))
-    //   } else {
-    //     result.push(this.state.valuesGiven[i]);
-    //     result.push(this.state.value[i]);
-    //   }
-    // }
-    // this.setState(() => ({
-    //   result,
-    // }))
-
-    const error = this.state.blanks.map((blank, i) => !this.state.value[i]);
-    this.setState({ error });
-
-    // this.setState({ showErrors: false })
-  }
+    let story = [];
+    let count = 0;
+    for (let i = 0; i < this.state.sentences.length - 1; i++) {
+      if (typeof this.state.value[i] === 'undefined') {
+        this.setState({ showErrors: false })
+        count++;
+      };
+    };
+    if (count === 1) {
+      for (let i = 0; i < this.state.sentences.length - 1; i++) {
+        story.push(this.state.sentences[i]);
+        story.push(this.state.value[i]);
+        this.setState(() => ({ story }));
+      };
+    };
+  };
 
   render() {
     return (
       <div className="content-area">
         <div className="form-container">
-          {!this.state.result &&
+          {!this.state.story &&
             <div className="form-title">
               <h1>MADLIBZ</h1>
               <h3>Fill in all the answers to recieve a story</h3>
             </div>
           }
           <div className='content'>
-            {this.state.blanks && !this.state.result && this.state.blanks.map((blank, index) =>
+            {this.state.blanks && !this.state.story && this.state.blanks.map((blank, index) =>
               <div className='blanks'>
-                {this.state.error[index] && <div>Please fill this out</div>}
-
-                {/* {!this.state.showErrors && !this.state.value[index] && <div>Please fill this out</div>} */}
+                {!this.state.showErrors && !this.state.value[index] && <div className="error">Please fill this out</div>}
                 <div className='blanksTitle'>{blank}:</div>
                 <input
                   onChange={(event) => { this.handleChange(event, index) }}
@@ -84,14 +78,14 @@ class Madlibz extends React.Component {
                   className={this.state.value[index]}
                 />
               </div>)}
-            {!this.state.result &&
+            {!this.state.story &&
               <button onClick={this.handleSubmit}>Submit</button>
             }
           </div>
-          {this.state.result &&
+          {this.state.story &&
             <div className="story">
               <h1>{this.state.title}</h1>
-              <div>{this.state.result}</div>
+              <div>{this.state.story}</div>
               <button onClick={this.newGame}>New Game</button>
             </div>
           }
@@ -102,8 +96,4 @@ class Madlibz extends React.Component {
 }
 
 export default Madlibz;
-//error handle / vali
 //react hooks
-//clean names !!!! very important : (
-//!!this.state.result.length
-//this.state.reult.length
